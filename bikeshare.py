e7f9af9809c7bb3863609eee3fa6119c2afb0b93
Emily <46229179+mamadoshi@users.noreply.github.com>
2022-01-31 13:13:43 -0700

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -26,7 +26,7 @@ def get_filters():
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filt4er
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some us bikeshare data!')
 
